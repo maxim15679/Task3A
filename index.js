69eedb69119ea4e43d0a3c10488e46d90c9d7f55
maxim15679 <maxim15679@mail.ru>
2016-11-11 01:29:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  var fullname = req.query.fullname;
+  var fullname = req.query.fullname.toLowerCase();
+
   fullname = fullname.trim();
+  console.log(fullname);
   //Check if normal
   if(!fullname)
     return res.send('Invalid fullname');
-  const re = new RegExp('[0-9_/$&+,:;=?@#|\'<>.^*()%!-]');
+  const re = new RegExp('[0-9_/$&+,:;=?@#|<>.^*()%!-]');
   const match = fullname.search(re);
-  //console.log(match);
+  while (searchspaces(fullname) != -1) {
+    fullname = fullname.replace(/\s\s/g, ' ');
+  }
+  console.log(fullname);
   if(match != -1){
     return res.send('Invalid fullname');
     console.log('IF');
@@ -21,23 +26,27 @@ app.get('/', (req, res) => {
     console.log('IF');
   } else {
     var response = arr[arr.length - 1];
+    var sub = response.substr(0,1);
+    response = sub.toUpperCase() + response.substr(1);
     var father;
     var name;
     if (arr.length - 2 >= 0) {
       father = arr[arr.length - 2];
+      father = father.toUpperCase();
   }else{
     father = '';
   }
 
   if (arr.length - 3 >= 0) {
     name = arr[arr.length - 3];
+    name = name.toUpperCase();
 }else{
   name = '';
 }
 if (name)
-  response = response + ' ' + name[0] + '.';
+  response = response + ' ' + name[0].toUpperCase() + '.';
 if (father)
-  response = response + ' ' + father[0] + '.';
+  response = response + ' ' + father[0].toUpperCase() + '.';
 res.send(response);
 }
 
@@ -46,3 +55,9 @@ res.send(response);
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+function searchspaces(str){
+  const re = new RegExp('(  )');
+  console.log(str.search(re));
+  return str.search(re);
+}

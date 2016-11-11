8581ed0f523999af613a82477a57ff6d86bc1364
maxim15679 <maxim15679@mail.ru>
2016-11-12 01:52:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,76 @@
+import fetch from 'node-fetch';
 import express from 'express';
+import S from 'express-statuses';
 
 const app = express();
 
+const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
+
+let pc = {};
+fetch(pcUrl)
+  .then(async (res) => {
+    pc = await res.json();
+    console.log(pc);
+  })
+.catch(err => {
+  console.log('Чтото пошло не так:', err);
+});
+
 app.get('/', (req, res) => {
-  var fullname = req.query.fullname.toLowerCase();
+  res.json(pc);
+});
 
-  fullname = fullname.trim();
-  console.log(fullname);
-  //Check if normal
-  if(!fullname)
-    return res.send('Invalid fullname');
-  const re = new RegExp('[0-9_/$&+,:;=?@#|<>.^*()%!-]');
-  const match = fullname.search(re);
-  while (searchspaces(fullname) != -1) {
-    fullname = fullname.replace(/\s\s/g, ' ');
-  }
-  console.log(fullname);
-  if(match != -1){
-    return res.send('Invalid fullname');
-    console.log('IF');
-  }
-  const arr = fullname.split(' ');
-  if (arr.length > 3 ) {
-    res.send('Invalid fullname');
-    console.log('IF');
-  } else {
-    var response = arr[arr.length - 1];
-    var sub = response.substr(0,1);
-    response = sub.toUpperCase() + response.substr(1);
-    var father;
-    var name;
-    if (arr.length - 2 >= 0) {
-      father = arr[arr.length - 2];
-      father = father.toUpperCase();
-  }else{
-    father = '';
-  }
+app.get('/volumes', (req, res) => {
+  const hdd = pc.hdd;
+  var volumes = {};
+  hdd.forEach((item) => {
+    if (volumes[item.volume]) {
+      volumes[item.volume] = volumes[item.volume] + parseInt(item.size, 10);
+    } else {
+      volumes[item.volume] = parseInt(item.size, 10);
+    }
+  });
+  var names = Object.getOwnPropertyNames(volumes).forEach((val) => {
+    volumes[val] = volumes[val] + 'B';
+  });
+  res.send(volumes);
+});
 
-  if (arr.length - 3 >= 0) {
-    name = arr[arr.length - 3];
-    name = name.toUpperCase();
-}else{
-  name = '';
-}
-if (name)
-  response = response + ' ' + name[0].toUpperCase() + '.';
-if (father)
-  response = response + ' ' + father[0].toUpperCase() + '.';
-res.send(response);
-}
-
+app.get('/:level1?/:level2?/:level3?/:level4?', (req, res, next) => {
+  //  res.send(req.params);
+  var result = pc[req.params.level1];
+  if (!result && result !== null && result !== 0 && result !== '') {
+    console.log('404');
+    res.status(404)        // HTTP status 404: NotFound
+        .send('Not found');
+  }
+  if (req.params.level2) {
+    if (!result[req.params.level2]) {
+      console.log('404');
+      res.status(404)        // HTTP status 404: NotFound
+          .send('Not found');
+    }
+    result = result[req.params.level2];
+  }
+  if (req.params.level3) {
+    if (!result[req.params.level3]) {
+      console.log('404');
+      res.status(404)        // HTTP status 404: NotFound
+          .send('Not found');
+    }
+    result = result[req.params.level3];
+  }
+  if (req.params.level4) {
+    if (!result[req.params.level4]) {
+      console.log('404');
+      res.status(404)        // HTTP status 404: NotFound
+          .send('Not found');
+    }
+    result = result[req.params.level4];
+  }
+  res.send(JSON.stringify(result));
 });
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
-
-function searchspaces(str){
-  const re = new RegExp('(  )');
-  console.log(str.search(re));
-  return str.search(re);
-}
